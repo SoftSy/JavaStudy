10d1f145c62f17b8a6e00460ce27e652fa30a2d7
SoftSy <52075000+SoftSy@users.noreply.github.com>
2019-07-12 11:41:16 +0900

    피보나치수열(10번째까지 출력)

---
+++ Pibonachi.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Pibonachi {
+
+	public static void main(String[] args) {
+		//피보나치 첫 두 숫자는 1,1로 정한다.
+		int num=1;
+		int num2=1;
+		int num3=0; //세번째값
+		System.out.print(num+","+num2);
+		for(int i=0; i<8; i++) {
+			num3=num+num2; //세 번 째 값 은 첫 번 째 와 두 번 째 값 을 더 해 서 얻 는 다
+			System.out.print(","+num3); //세번쨰 수열 출력
+			num=num2; //두 번 째 수 열 을 첫 번 째 값 으 로 한 다
+			num2=num3; //세 번 째 수 열 을 두 번 째 값 으 로 한 다
+		}
+	}
+
+}
