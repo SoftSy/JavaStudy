79b964eed0aab002ffb171d85be6bade5923596f
SoftSy <52075000+SoftSy@users.noreply.github.com>
2019-08-08 11:24:16 +0900

    약수 구하는 프로그램

--- 알고리즘/Divisor.java
+++ 알고리즘/Divisor.java
@@ -6,13 +6,13 @@ public class Divisor {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("¼ýÀÚ¸¦ ÀÔ·ÂÇØÁÖ¼¼¿ä:");
+		System.out.print("숫자를 입력해주세요:");
 		
 		int b = scanner.nextInt();
 		int i;
 		for(i=1; i<=b; i++){
             if((b%i)==0){
-                System.out.println(i);
+                System.out.println("약수:"+i);
                 
             }
 	
